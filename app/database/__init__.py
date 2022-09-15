f59373f5a258943c22968678c3dd0efa513e33fa
Ubuntu <ubuntu@ip-172-31-1-112.us-west-1.compute.internal>
2022-09-15 09:13:26 +0000

    fix requirements

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -4,7 +4,8 @@ from pymongo.server_api import ServerApi
 try:
     conn = MongoClient("mongodb+srv://mongo:hehe@cluster0.2rd5qqp.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
     print("INFO\tDatabase Connected successfully!!!")
-except:  
+except Exception as e:  
     print("INFO\tCould not connect to MongoDB")
+    print("ERROR\t",e)
 
 db = conn['inagri_test']
